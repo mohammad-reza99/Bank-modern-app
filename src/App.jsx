e3a2f2b12c9bb06cc8c2fd1e05c58040e7237a86
Mohammad Reza Zaeri <rezazaeri1990@gmail.com>
2025-09-11 17:08:54 +0200

    Add new components and assets for the application
    - Created new SVG assets: menu.svg, quotes.svg, twitter.svg
    - Added new PNG assets: people01.png, people02.png, people03.png, robot.png
    - Implemented new React components: Billing, Business, Button, CTA, CardDeal, Clients, FeedbackCard, Footer, GetStarted, Hero, Navbar, Stats, Testimonials
    - Updated index.js to export new components
    - Added constants for navigation links, features, feedback, stats, footer links, social media, and clients
    - Enhanced index.css with new styles and gradients
    - Introduced style.js for layout and styling constants

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+import { useState } from "react";
+import styles from "./style";
+import {
+  Billing,
+  Navbar,
+  Business,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+  Hero,
+  Stats,
+} from "./components";
+const App = () => (
+  <div className="bg-gray-900 w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
 
-export default App
+    <div className={`bg-gray-900 ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    <div className={`bg-gray-900 ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
